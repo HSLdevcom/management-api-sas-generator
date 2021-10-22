b5c8a0c85533859ee183f9bcc2de86a8c7b32fad
Joel Lappalainen <lappalj8@gmail.com>
2021-10-22 10:58:35 +0300

    Add logging

--- HttpTrigger.cs
+++ HttpTrigger.cs
@@ -1,7 +1,7 @@
 using System;
 using System.IO;
-using System.Text;   
-using System.Globalization;   
+using System.Text;
+using System.Globalization;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -18,20 +18,22 @@ namespace Digitransit.Function
         [FunctionName("HttpTrigger")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            ILogger log)
+            ILogger log, ClaimsPrincipal principal)
         {
+            var roles = principal.Claims.Where(e => e.Type == "roles").Select(e => e.Value);
             log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation(roles);
 
-            var id = "integration";   
-            var key = Environment.GetEnvironmentVariable("KEY");   
-            var expiry = DateTime.UtcNow.AddHours(1);   
+            var id = "integration";
+            var key = Environment.GetEnvironmentVariable("KEY");
+            var expiry = DateTime.UtcNow.AddHours(1);
             string encodedToken;
-            using (var encoder = new HMACSHA512(Encoding.UTF8.GetBytes(key)))   
-            {   
-                var dataToSign = id + "\n" + expiry.ToString("O", CultureInfo.InvariantCulture);   
-                var hash = encoder.ComputeHash(Encoding.UTF8.GetBytes(dataToSign));   
-                var signature = Convert.ToBase64String(hash);   
-                encodedToken = string.Format("SharedAccessSignature uid={0}&ex={1:o}&sn={2}", id, expiry, signature);  
+            using (var encoder = new HMACSHA512(Encoding.UTF8.GetBytes(key)))
+            {
+                var dataToSign = id + "\n" + expiry.ToString("O", CultureInfo.InvariantCulture);
+                var hash = encoder.ComputeHash(Encoding.UTF8.GetBytes(dataToSign));
+                var signature = Convert.ToBase64String(hash);
+                encodedToken = string.Format("SharedAccessSignature uid={0}&ex={1:o}&sn={2}", id, expiry, signature);
             }
 
             string responseMessage = encodedToken;
